552483bd41f395fec655787db506e566a5da88bf
yousif <yousif.dhneem@gmail.com>
2023-12-27 23:11:55 +0300

    Files are prepared in be

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+// Require dependencies
+const express = require('express');
+require('dotenv').config()
+
+// connect to mongoDB
+require('./config/db')
+
+// initialize express app
+const app = express();
+
+// get the port number form .env file, if undefined, 3000
+const port = process.env.PORT || 3000
+
+
+//  Middlewares
+// Templating Engine
+
+
+// to encode req.body - make form data readable in controllers
+app.use(express.urlencoded({ extended: true }));
+
+// link you static folder i.e. images, css 
+app.use(express.static('public'));
+//-------------------------//
+
+
+//------- Mount routes -------//
+// Your code goes here
+
+
+//-------------------------//
+
+// start listening to requests coming from the PORT
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
