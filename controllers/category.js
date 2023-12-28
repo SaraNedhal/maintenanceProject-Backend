2314ba9bb81c41e0bdc8deae00f0613823b93ed5
yousif <yousif.dhneem@gmail.com>
2023-12-28 17:01:35 +0300

    finished category Api's with routes and tested without relationships

--- controllers/category.js
+++ controllers/category.js
@@ -1,1 +1,62 @@
 const {Category} = require("../models/Category");
+
+//restFul Api's
+
+exports.category_create_post = (req, res)=>{
+console.log(req.body)
+let category = new Category(req.body);
+
+// Save Category
+category.save()
+.then((category)=>{
+    res.json({category})
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+}
+
+exports.category_index_get = (req, res)=>{
+Category.find()
+.then((categories)=>{
+    res.json({categories})
+
+})
+.catch((err)=>{
+ console.log(err)
+})
+}
+
+exports.category_delete_get = (req, res)=>{
+ console.log(req.query.id);
+ Category.findByIdAndDelete(req.query.id)
+ .then((category)=>{
+   res.json({category})
+ })
+ .catch((err)=>{
+    console.log(err);
+ })
+}
+
+exports.category_edit_get = (req, res) =>{
+    Category.findById(req.query.id)
+    .then((category)=>{
+      res.json({category})
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+  
+}
+
+exports.category_update_put = (req, res)=> {
+  console.log(req.body.id)
+  Category.findByIdAndUpdate(req.body._id, req.body,{new:true})
+  .then((category)=>{
+    res.json({category})
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
--- routes/category.js
+++ routes/category.js
@@ -10,6 +10,11 @@ const categoryCtrl = require("../controllers/category");
 
 //Routes 
 
+router.post("/add",  categoryCtrl.category_create_post);
+router.get("/index", categoryCtrl.category_index_get);
+router.delete("/delete", categoryCtrl.category_delete_get);
+router.get("/edit", categoryCtrl.category_edit_get);
+router.put("/update", categoryCtrl.category_update_put);
 
 
 //Export 
