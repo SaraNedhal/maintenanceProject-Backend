dfd4a3ed881a249312e7515576d00026da1d1b73
saranedhal <sarahnedhal@gmail.com>
2023-12-28 16:32:12 +0300

    added an attribute to the order model

--- routes/request.js
+++ routes/request.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const router = require('express').Router();
-
+const express = require("express");
+const router = require("express").Router();
 
 router.use(express.json());
 
-//Import Request Controller 
+//Import Request Controller
 
 const requestCtrl = require("../controllers/request");
 
-//Routes 
-router.get('/index' , requestCtrl.request_index_get);
-router.post('/add' , requestCtrl.request_create_post);
-router.get('/edit' , requestCtrl.request_edit_get);
-router.post('/update' , requestCtrl.request_update_post);
-router.delete('/delete' , requestCtrl.request_delete_get);
-//Export 
+//Routes
+router.get("/index", requestCtrl.request_index_get);
+router.post("/add", requestCtrl.request_create_post);
+router.get("/edit", requestCtrl.request_edit_get);
+router.post("/update", requestCtrl.request_update_post);
+router.delete("/delete", requestCtrl.request_delete_get);
+//Export
 module.exports = router;
