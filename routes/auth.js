edc3593725e3b8fbaa55ca83f629eb5935ed811d
yousif <yousif.dhneem@gmail.com>
2024-01-02 14:47:32 +0300

    added one user show gt api

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,8 @@ router.post("/signup", authCtrl.auth_signup_post);
 router.post("/signin", authCtrl.auth_signin_post);
 router.get("/user", authCtrl.auth_user_index_get);
 router.get("/user/edit", authCtrl.auth_user_edit_get);
+router.get("/user/profile", authCtrl.auth_user_show_get);
+
 router.put("/user/update", authCtrl.auth_user_update_put);
 
 
