e21578c1777911dd5127fde8f8b13224f58dcfc4
lulwa <lulwafelaifel9@gmail.com>
2024-01-02 11:10:29 +0300

    order index-request

--- routes/order.js
+++ routes/order.js
@@ -11,6 +11,7 @@ const orderCtrl = require("../controllers/order");
 //Routes 
 //create post
 router.post("/add",orderCtrl.order_create_post);
+router.get("/index",orderCtrl.order_index_get);
 router.get("/edit",orderCtrl.order_edit_get);
 router.post("/update",orderCtrl.order_update_post);
 router.delete("/delete",orderCtrl.order_delete_get);
